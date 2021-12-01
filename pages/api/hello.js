879faa72f662c117cd0387f00cad6a05eea5b479
GarfDev <garfdev.13@gmail.com>
2021-12-01 13:35:21 +0700

    feat: integrate with hashlib art generator

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { startCreating, buildSetup } from '../../core/hashlips/src/main';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  await buildSetup();
+  const result = await startCreating();
+  res.status(200).json({ result })
 }
